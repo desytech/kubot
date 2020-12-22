c9bf1950dd31f4426aed78119507afd4aeb983af
Desy <m.huber@huber-its.de>
2020-12-22 13:20:00 +0100

    remove ETC from currency schema, we only support USDT for now

--- schemas/config.py
+++ schemas/config.py
@@ -2,7 +2,7 @@ from schema import Schema, Or, And
 
 currencies = Schema([
     {
-        'currency': Or("USDT", "ETC"),
+        'currency': Or("USDT"),
         'term': Or(7, 14, 28),
         'reserved_amount': And(int, lambda a: a >= 0)
     }
