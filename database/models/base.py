ada0f892a5ba501a59761de60ab28289b447f977
Desy <m.huber@huber-its.de>
2021-01-04 15:14:38 +0100

    add provisioning of kubot overview dashboard #12

--- database/models/base.py
+++ database/models/base.py
@@ -2,8 +2,8 @@ from peewee import *
 from playhouse.pool import PooledPostgresqlExtDatabase
 
 db = PooledPostgresqlExtDatabase('kubot', user='kubot',
-                           password='kubot', host='localhost',
-                           port=5433, max_connections=8, stale_timeout=300)
+                           password='kubot', host='postgres',
+                           port=5432, max_connections=8, stale_timeout=300)
 
 class BaseModel(Model):
     class Meta:
